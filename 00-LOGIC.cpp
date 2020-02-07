3ea143d74f6a6fb8a3032d24f85e78a4461ca31b
KinRobert <60378875+KinRobert@users.noreply.github.com>
2020-02-07 23:11:41 +0800

    Add files via upload

---
+++ 00-LOGIC.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class Logic{
+	private:
+		int num;
+	public:
+		void input();
+		void conjunction();
+		void disjunction();
+		void negation();
+		void conditional();
+		void biconditional();
+};
+
+int main(int argc, char** argv) {
+	Logic logic;
+	logic.input();
+
+	return 0;
+}
+
+void Logic::input(){
+	cout<<"Choose a number \n 1.Conjunction	2.Disjunction	3.Negation \n 4.Conditional 	5.Biconditional	6.Exit";
+	cin >> num;
+	
+	switch(num){
+		case 1: conjunction(); break;
+		case 2: disjunction(); break;
+		case 3: negation(); break;
+		case 4: conditional(); break;
+		case 5: biconditional(); break;
+		case 6: cout << "Terminated";
+	}
+	
+}
+void Logic::conjunction(){
+	string q,p;
+		
+		cout << "\n 	Conjunction. The conjunction p ^ q are true if and only if p and q are both true";
+		cout<< "\n 	Enter the value of p	";
+		cin>>p;
+		cout<<"	Enter the value of q	";
+		cin>>q;
+		
+		if( q == "true" && p =="true" )
+		 {
+			cout<<"\n p ^ q = true";
+		}
+		
+		else cout<< ("\n	 p ^ q = false");	
+}
+void Logic::disjunction(){
+	string q,p;
+		
+		cout << "\n 	Disjunction. The disjunction  p or q it true if  p and q are both true, or one of them is true";
+		cout<< "\n 	Enter the value of p	";
+		cin>>p;
+		cout<<"	Enter the value of q	";
+		cin>>q;
+		
+		if( q == "false" && p =="false")
+		 {
+			cout<<"\n p or q = false";
+		}
+		
+		else cout<< ("\n	 p or q = true");
+}
+void Logic::negation(){
+	string q;
+			cout<<"\n 	Negation. The negation not p means that the value is the opposite of the original value";
+			cin>>q;
+			
+			if(q=="true") cout << "\n	q= false";
+			else cout<<"\n q=true";	
+}
+void Logic::conditional(){
+	string q,p;
+			cout<<"\n	Conditional. The conditional q then p, written q-->p is false if q is true and p is false";
+			cout<< "\n 	Enter the value of p	";
+			cin>>p;
+			cout<<"	Enter the value of q	";
+			cin>>q;
+			
+			if(p=="true" && q=="false") cout <<"q-->p is false";
+			else "q--p is true";
+}
+void Logic::biconditional(){
+	string q,p;
+			cout<<"\n	The Biconditional p<-->q is true if both are true or both are false";
+			cout<< "\n 	Enter the value of p	";
+			cin>>p;
+			cout<<"	Enter the value of q	";
+			cin>>q;
+			
+			if((p=="true" && q=="true")||(p=="false" && q=="false"))
+			cout << "\n 	The Biconditional p<-->q is true ";
+			else cout << "\n 	The Biconditional p<-->q is false ";
+}
---
+++ 01-SETS.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class Sets{
+	private:
+		int set1[100], set2[100], set3[100];
+		int c1=0, c2=0, c3=0;
+		int temp=0;
+	
+	public:
+		void input();
+		void intersection();
+		void print_set(int set[], int size);
+		void unite();
+		void compliment(string str,int setA[],int setB[], int A, int B);
+		void operation(int setA[],int setB[], int A, int B);
+		void relative_compliment();
+};
+
+int main(int argc, char** argv) {
+	Sets sets;
+	sets.input();
+	
+	return 0;
+}
+void Sets::input(){
+	cout<< "\n Enter the values of first set	";
+			for(;;){	
+ 				cin>> temp;
+				if(temp==0) break;
+				else{
+				set1[c1]=temp;		
+				c1++;
+				}
+		
+			}
+	
+
+			cout<< "\n Enter the values of second set	";
+			for(int a=0; a<100; a++){
+				cin>> temp;
+				if(temp==0) break;
+				else{
+				set2[c2]=temp;		
+				c2++;
+				}
+			}
+	
+			
+		 cout<< "SETS OPERATION \n\n\n";
+		 cout<< "\n\n The intersection of 2 sets \n";
+		 intersection();
+		 print_set(set3, c3);
+		 unite();
+		 compliment("\n\n The complement of set2 to set1",set1,set3,c1,c3); 
+		 compliment("\n\n The complement of set1 to set2",set2,set3,c2,c3); 
+		 
+		 cout<<"\n\nThe relative Compliment of set 1 to set 2\n";
+		 print_set(set3, c3);
+		 compliment("",set1,set3,c1,c3); 
+		 cout<<"\n\nThe relative Compliment of set 1 to set 2\n";
+		 print_set(set3, c3);
+		 compliment("",set2,set3,c2,c3);
+		 
+		 cout<<"\n\n the symmetric difference of set1 and set2";
+		 compliment("",set1,set3,c1,c3);
+		 compliment("",set2,set3,c2,c3);
+}
+void Sets::intersection(){
+		for(int a=0; a<c1; a++){
+			for(int b=0; b<c2; b++){
+				if(set1[a]==set2[b]) {
+					set3[c3]=set1[a];
+					c3++;
+				}
+			}
+		}
+}
+void Sets::print_set(int set[], int size){
+		for(int a=0; a<size; a++ ){
+			cout<<" "<<set[a];
+		}
+}
+void Sets::compliment(string str,int setA[],int setB[], int A, int B){
+	cout<<str<<endl;
+		operation(setA,setB,A,B);
+}
+void Sets::operation(int setA[],int setB[], int A, int B){
+	int flag=0;
+		for(int a=0; a<A; a++){
+			for(int b=0; b<B; b++){
+				if(setA[a]==setB[b]) flag=1;
+			}
+			if(flag!=1) cout<< " " << setA[a];
+			else flag=0;
+		}
+}
+void Sets::unite(){
+	int flag=0;		
+		cout<< "\n The union of two sets \n";
+		operation(set1,set3,c1,c3);
+		print_set(set3, c3);
+		operation(set2,set3,c2,c3);
+}
+
